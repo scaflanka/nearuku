f349a954f71a1bcb39c24108d35f650accfd34ee
Sadeepa11 <srisadeepa658@gmail.com>
2026-06-25 09:01:07 +0530

    fix: downgrade @rnmapbox/maps to version 10.2.10 for compatibility

--- app.config.js
+++ app.config.js
@@ -1,9 +1,18 @@
-﻿const appJson = require('./app.json');
+const appJson = require('./app.json');
 
 module.exports = () => {
   const plugins = appJson.expo.plugins.map((plugin) => {
     if (plugin === '@rnmapbox/maps') {
       return ['@rnmapbox/maps', { RNMapboxMapsDownloadToken: process.env.MAPBOX_DOWNLOAD_TOKEN }];
+    }
+    if (Array.isArray(plugin) && plugin[0] === '@rnmapbox/maps') {
+      return [
+        '@rnmapbox/maps',
+        {
+          ...plugin[1],
+          RNMapboxMapsDownloadToken: process.env.MAPBOX_DOWNLOAD_TOKEN,
+        },
+      ];
     }
     return plugin;
   });
